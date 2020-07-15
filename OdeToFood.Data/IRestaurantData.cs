7a3055e2c5f88bfeaad1fa0c41bba5ad8a233e57
Yan P Aung <yan.pye.aung@gmail.com>
2020-07-14 20:53:54 -0700

    Edit and Add new restaurant data; module 4 done

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -8,6 +8,9 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        int Commit();
+        Restaurant Add(Restaurant restaurant);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -26,6 +29,30 @@ namespace OdeToFood.Data
 
         public Restaurant GetById(int id)
             => restaurants.SingleOrDefault(r => r.Id == id);
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
             => from r in restaurants
